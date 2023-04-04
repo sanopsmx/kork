ef6357c279c8b401488ab85226f8f043223e8d32
aman-agrawal <9412470@gmail,com>
2023-04-04 17:46:16 +0530

    Fixed ProviderRegistry exception and other issues

--- kork-web/src/main/groovy/com/netflix/spinnaker/config/OkHttpClientComponents.groovy
+++ kork-web/src/main/groovy/com/netflix/spinnaker/config/OkHttpClientComponents.groovy
@@ -25,7 +25,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-import javax.inject.Provider
+import jakarta.inject.Provider
 
 @Configuration
 @EnableConfigurationProperties([OkHttpClientConfigurationProperties, OkHttpMetricsInterceptorProperties])

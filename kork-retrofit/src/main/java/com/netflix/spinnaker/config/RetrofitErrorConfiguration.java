e0ef79acf4f6cbf370735e0de525b5f5e569bf66
SusmithaGundu <susmitha.gundu@opsmx.io>
2023-04-19 12:36:06 +0530

    fix for RetrofitServiceFactory bean not being created issue

--- kork-retrofit/src/main/java/com/netflix/spinnaker/config/RetrofitErrorConfiguration.java
+++ kork-retrofit/src/main/java/com/netflix/spinnaker/config/RetrofitErrorConfiguration.java
@@ -19,9 +19,11 @@ package com.netflix.spinnaker.config;
 import com.netflix.spinnaker.kork.retrofit.exceptions.SpinnakerRetrofitExceptionHandlers;
 import com.netflix.spinnaker.kork.web.exceptions.ExceptionMessageDecorator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.netflix.spinnaker.kork.retrofit")
 public class RetrofitErrorConfiguration {
   @Bean
   SpinnakerRetrofitExceptionHandlers spinnakerRetrofitExceptionHandlers(

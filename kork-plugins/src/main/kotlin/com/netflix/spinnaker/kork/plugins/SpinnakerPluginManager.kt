78539e5430b98e2043057ef750f4545eadbf81e7
Sanjeev Thatiparthi <54054932+sanopsmx@users.noreply.github.com>
2023-06-29 17:07:45 +0530

    Oes 1.30.x master adhoc (#70)
    * added more log statements to check.
    
    * added log statements to print the values.

--- kork-plugins/src/main/kotlin/com/netflix/spinnaker/kork/plugins/SpinnakerPluginManager.kt
+++ kork-plugins/src/main/kotlin/com/netflix/spinnaker/kork/plugins/SpinnakerPluginManager.kt
@@ -93,10 +93,10 @@ open class SpinnakerPluginManager(
     // system version is null.
     return serviceVersion.resolve().let {
       if (it == ServiceVersion.UNKNOWN_VERSION || it.isEmpty()) {
-        log.info(" setting the service version to default: if block : " , it)
+        log.info(" setting the service version to default: if block : {}" , it)
         ServiceVersion.DEFAULT_VERSION
       } else {
-        log.info(" not setting the service version to default: else block : ", it)
+        log.info(" not setting the service version to default: else block : {}", it)
         it
       }
     }
@@ -147,6 +147,6 @@ open class SpinnakerPluginManager(
 
   init {
     systemVersion = getSystemVersion()
-    log.info(" system version : " , systemVersion)
+    log.info(" system version : {}" , systemVersion)
   }
 }
